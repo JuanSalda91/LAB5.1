5d6fdc9169b1738b430b7c136e32733d52317743
JuanSalda91 <juansalda9131@gmail.com>
2025-11-11 23:59:02 -0500

    remove item button fixed and moved to initial add product function

--- js/app.js
+++ js/app.js
@@ -12,20 +12,30 @@ function updateTotalPrice(amount) {
   totalPriceSpan.textContent = totalPrice.toFixed(2);
 }
  
-// Function to remove an item
+// Function to remove an item // --- total price update and empty cart check --- //
 function removeItem(event) {
   const item = event.target.closest('li');
   const price = parseFloat(item.dataset.price);
   updateTotalPrice(-price);
+  const quantity = parseInt(item.dataset.quantity) || 1;
+
+  // --- substract full price (price * quantity) when the item is removed --- //
+  updateTotalPrice(-price*quantity);
   item.remove();
+
+  // --- reset total price when cart is empty --- //
+  if(cart.children.length === 0) {
+    totalPrice = 0;
+    totalPriceSpan.textContent = totalPrice.toFixed(2);
+  }
 }
 
-// ----- Function to add product to the cart ----- //
+// ----- 1. Function to add product to the cart ----- //
 function addProduct() {
     const name = productNameInput.value.trim();
     const price = parseFloat(productPriceInput.value);
 
-    // ----- condition to ensure product name and price ------ //
+    // ----- 3. condition to ensure product name and price ------ // --- created with task 1 --- //
     if(!name) {
         alert('Please enter product name');
         return;
@@ -35,10 +45,17 @@ function addProduct() {
         return;
     }
 
-    // ----- create list of products ----- //
+    // --- create list of products --- //
     const li = document.createElement('li');
     li.textContent = `${name} - $${price.toFixed(2)}`; // --- first $ sign will appear as the currency symbol without affecting the code --- //
     li.dataset.price = price;
+
+    // ----- 2. remove items from cart ----- //
+    const removeBtn = document.createElement('button');
+    removeBtn.textContent = 'Remove';
+    removeBtn.addEventListener('click', removeItem);
+    li.appendChild(removeBtn)
+
 
     // ----- append to cart ----- //
     cart.appendChild(li);
